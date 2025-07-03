7145848ea3abc711fa321fab0312882366867705
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-07-03 12:13:30 -0500

    Add skeleton project

---
+++ IdiomsApp/IdiomsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdiomsAppApp.swift
+//  IdiomsApp
+//
+//  Created by Dmitry Volkov on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct IdiomsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
